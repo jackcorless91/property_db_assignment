2bef25a548369cb268f4244749d5f584d920ec6a
Jack Corless <jackcorless91@gmail.com>
2024-11-02 16:53:21 +1100

    created properties table with agent foreign key

--- property.sql
+++ property.sql
@@ -17,8 +17,17 @@ CREATE TABLE clients(
   msisdn INTEGER NOT NULL,
   email VARCHAR(100) NOT NULL,
   budget INTEGER
-)
+);
 
-
+CREATE TABLE properties_id(
+  property_id SERIAL PRIMARY KEY,
+  property_address VARCHAR(100) NOT NULL,
+  postcode INTEGER NOT NULL,
+  property_type VARCHAR(100) NOT NULL,
+  rooms VARCHAR(100) NOT NULL,
+  size_sqm INTEGER NOT NULL,
+  asking_price INTEGER NOT NULL,
+  sale_price INTEGER
+);
 
 
