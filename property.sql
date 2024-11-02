dde8a9150d9161d426a829c303641b088f9b2913
Jack Corless <jackcorless91@gmail.com>
2024-11-02 17:30:18 +1100

    added comparison operators for exisiting data constraints, fixed foreign key

--- property.sql
+++ property.sql
@@ -1,38 +1,40 @@
 CREATE DATABASE property_db;
 
+DROP TABLE IF EXISTS properties;
 DROP TABLE IF EXISTS real_estate_agents;
 DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS properties;
+
 
 CREATE TABLE real_estate_agents(
-  agent_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
-  msisdn INTEGER NOT NULL,
+  msisdn INTEGER NOT NULL CHECK (msisdn = 11),
   email VARCHAR(100) NOT NULL,
-  salary INTEGER NOT NULL
+  salary INTEGER NOT NULL NOT NULL CHECK (salary >= 1)
 );
 
 CREATE TABLE clients(
-  client_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
-  msisdn INTEGER NOT NULL,
+  msisdn INTEGER NOT NULL CHECK (msisdn = 11),
   email VARCHAR(100) NOT NULL,
   budget INTEGER
 );
 
 CREATE TABLE properties(
-  property_id SERIAL PRIMARY KEY,
-  property_address VARCHAR(100) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  address VARCHAR(100) NOT NULL,
   postcode INTEGER NOT NULL,
   property_type VARCHAR(100) NOT NULL,
-  rooms VARCHAR(100) NOT NULL,
+  rooms INTEGER NOT NULL CHECK (rooms >= 1),
+  bathrooms INTEGER NOT NULL CHECK (bathrooms >= 1)
   size_sqm INTEGER NOT NULL,
   asking_price INTEGER NOT NULL,
   sale_price INTEGER,
   agent_id INTEGER NOT NULL,
-  FOREIGN KEY (agent_id) REFERENCES clients(agent_id) ON DELETE CASCADE
+  FOREIGN KEY (agent_id) REFERENCES real_estate_agents(id) ON DELETE CASCADE
 );
 
 
