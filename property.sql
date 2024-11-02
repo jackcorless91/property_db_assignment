7b0788b81e6bd2709cacc75b1fa46ab526dde5c5
Jack Corless <jackcorless91@gmail.com>
2024-11-02 18:13:29 +1100

    created sales and commission tables, added foreign keys

--- property.sql
+++ property.sql
@@ -29,12 +29,32 @@ CREATE TABLE properties(
   postcode INTEGER NOT NULL,
   property_type VARCHAR(100) NOT NULL,
   rooms INTEGER NOT NULL CHECK (rooms >= 1),
-  bathrooms INTEGER NOT NULL CHECK (bathrooms >= 1)
-  size_sqm INTEGER NOT NULL,
+  bathrooms INTEGER NOT NULL CHECK (bathrooms >= 1),
+  size_sqm INTEGER NOT NULL CHECK (size_sqm >= 1),
   asking_price INTEGER NOT NULL,
   sale_price INTEGER,
   agent_id INTEGER NOT NULL,
   FOREIGN KEY (agent_id) REFERENCES real_estate_agents(id) ON DELETE CASCADE
 );
 
+CREATE TABLE sales(
+  id SERIAL PRIMARY KEY,
+  sale_date DATE NOT NULL,
+  sale_price INTEGER NOT NULL,
+  property_id INTEGER NOT NULL,
+  client_id INTEGER NOT NULL,
+  agent_id INTEGER NOT NULL,
+  FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE,
+  FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE,
+  FOREIGN KEY (agent_id) REFERENCES real_estate_agents(id) ON DELETE CASCADE
+);
 
+CREATE TABLE commission(
+  id SERIAL PRIMARY KEY,
+  commission_amount INTEGER,
+  payment_date DATE,
+  agent_id INTEGER NOT NOT NULL,
+  sale_id INTEGER NOT NULL, 
+  FOREIGN KEY (agent_id) REFERENCES real_estate_agents(id) ON DELETE CASCADE,
+  FOREIGN KEY (sale_id) REFERENCES sales(id) ON DELETE CASCADE
+)
